2453e05e26893856a1a3983c5d3d3b123d086d3e
zairacodes <163322374+zairacodes@users.noreply.github.com>
2025-01-18 12:39:47 +0000

    Initialise Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+  return (
+    <footer>
+      <p>&copy; 2025 Exhibition Curator. All Rights Reserved.</p>
+      <p>
+        Designed by{" "}
+        <a href="https://github.com/zairacodes" target="_blank">
+          zairacodes
+        </a>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
