6d5e4eb6e9937aae585c032471459e1ca1c39d13
zairacodes <163322374+zairacodes@users.noreply.github.com>
2025-01-18 12:46:41 +0000

    Update Routes for Home and MyExhibitions pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import MyExhibitions from "./pages/MyExhibitions";
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/" />
-        <Route path="/home" />
-        <Route path="/myexhibitions" />
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/myexhibitions" element={<MyExhibitions />} />
       </Routes>
       <Footer />
     </BrowserRouter>
