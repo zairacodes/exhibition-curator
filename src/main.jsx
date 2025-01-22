9162fd18d59eed52337affaad76b896c99c49290
zairacodes <163322374+zairacodes@users.noreply.github.com>
2025-01-22 19:06:20 +0000

    Initialise default exhibition in localStorage if not already present

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+const defaultExhibition = {
+  id: "default",
+  name: "My First Exhibition",
+  artworks: [],
+};
+
+const initialiseExhibitions = () => {
+  const storedExhibitions = JSON.parse(localStorage.getItem("exhibitions"));
+  if (!storedExhibitions) {
+    localStorage.setItem("exhibitions", JSON.stringify([defaultExhibition]));
+  }
+};
+
+initialiseExhibitions();
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
