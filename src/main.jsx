aa787ef04efdf99f238b60f20032c64bf89b3a3d
zairacodes <163322374+zairacodes@users.noreply.github.com>
2025-01-23 16:32:20 +0000

    Remove initialisation of default exhibition in localStorage

--- src/main.jsx
+++ src/main.jsx
@@ -3,21 +3,6 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
-const defaultExhibition = {
-  id: "default",
-  name: "My First Exhibition",
-  artworks: [],
-};
-
-const initialiseExhibitions = () => {
-  const storedExhibitions = JSON.parse(localStorage.getItem("exhibitions"));
-  if (!storedExhibitions) {
-    localStorage.setItem("exhibitions", JSON.stringify([defaultExhibition]));
-  }
-};
-
-initialiseExhibitions();
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
